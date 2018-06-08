d1becfa8d084e162f5fc05c5b129a03c8b660b9c
an636 <anandganeshgx97@gmail.com>
2018-06-09 00:46:47 +0530

    basic website login

---
+++ authen/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^register/', views.register),
+    url(r'^login/', views.login),
+    url(r'^logout/', views.logout),
+    # url(r'^dashboard/', views.dashboard),
+    url(r'^forgotpassword/', views.forgotpassword),
+    url(r'^(?P<p>[\w\-\_]+)/resetpassword/$',views.resetpassword),
+    url(r'^viewprofile/(?P<p>[\w]+)', views.viewprofile),
+    url(r'^viewprofile', views.viewprofile),
+     url(r'^topics', views.topics),
+      url(r'^myaccount', views.myaccount),
+]
